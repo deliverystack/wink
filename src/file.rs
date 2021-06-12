18a5300695ee561a70e0270801e99486e29d3306
commodore73 <git@c73.me>
2021-06-12 16:40:49 -0700

    Added documentation

--- src/file.rs
+++ src/file.rs
@@ -1,6 +1,20 @@
+//! The wink.file module contains a function for reading a file into a string,
+//! which is helpful for handling cases where the file does not exist or cannot be read.
+
+//TODO: this logic belongs in a library, not wink
+
 use std::io::Read;
 
-// read the specified file into a string
+/// Example usage:
+/// ```
+/// match file::read_file(&path) {
+///   Ok(contents) => {
+///     let deserialized: InvocableCategoryList = serde_json::from_str(&contents).unwrap();
+///   }
+/// }
+/// ```
+
+/// Read the specified file into a string.
 pub fn read_file(path: &str) -> Result<String, std::io::Error> {
     let mut file = std::fs::File::open(path)?;
     let mut data = String::new();
