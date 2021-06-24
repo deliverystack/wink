91b0cd32f15b70c9fd4a15dd99679416ec02520b
commodore73 <git@c73.me>
2021-06-24 16:19:02 -0700

    comments

--- src/main.rs
+++ src/main.rs
@@ -3,32 +3,24 @@
 // "get" => Invoker::cmd("echo"), // Windows File System explorer //TODO: rename echo?
 //        "exp" => Invoker::exp("", &[]),    // Windows File System explorer
 
-// $ wink gowindow
-// wslpath: C:/temp/GoWindow.{ED7BA470-8E54-465E-825C-99712043E01C}: No such file or directory
-
-// /mnt/c/usr/bin/xeyes - wslpath seems to be adding path incorrectly
-
 //! wink is a command line tool that provides access to Windows and Windows Subsystem for Linux (WSL) features and programs.
-
 //! wink provides a simple interface for invoking almost any Windows or WSL feature
 //! using cmd.exe /c, using explorer.exe, using bash.exe, or by invoking the executable directly.
-
 //! wink uses cargo, so you can use cargo build to build wink. You can also use the wince script to build wink.
-
-//! ```//TODO: less hard-coding in wince build script```
-
 //! Run wink with no command line parameters to get usage information.
-
-use wink::wsl::inv::invocablecategorylist::InvocableCategoryList;
 
 /// The main() function of the program accepts command line arguments through env::args.collect()
 /// rather than as parameters.
 fn main() {
-    let category_list = InvocableCategoryList::get();
-    let config = wink::WinkConfig::new(std::env::args().collect());
+    // get a list of categories containing invocable commands
+    // defined in /wsl/inv/invocablecategorylist.rs
+    let category_list = wink::wsl::inv::invocablecategorylist::InvocableCategoryList::get();
+    let config = wink::winkconfig::WinkConfig::new(std::env::args().collect()); // parse command line arguments
 
+    // if successful parsing command line, the Ok enum value contains the WinkConfig
+    // otherwise, Err enum value contains the WinkConfig and a HelpError
     std::process::exit(match config {
-        Ok(config) => wink::run(config, category_list),
+        Ok(config) => wink::run(config, category_list), // what if this raises an error
         Err((config, e)) => wink::help(&e.to_string(), config, category_list.categories),
     });
 }
